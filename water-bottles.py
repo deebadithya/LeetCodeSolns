11ff6d053d102b79ae61699ec72ebeeced91617b
[deeba adithya] <2016016@saec.ac.in>
2024-07-07 16:05:45 +0530

    adding water-bottles.py

---
+++ water-bottles.py
@@ -0,0 +1,22 @@
+"""
+water-bottles.py
+1518. Water Bottles
+Solved
+Easy
+Topics
+Companies
+Hint
+There are numBottles water bottles that are initially full of water. You can exchange numExchange empty water bottles from the market with one full water bottle.
+The operation of drinking a full water bottle turns it into an empty bottle.
+Given the two integers numBottles and numExchange, return the maximum number of water bottles you can drink.
+"""
+
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        counts = numBottles
+        while numBottles >= numExchange:
+           temp = numBottles // numExchange
+           counts += temp
+           numBottles = temp + numBottles % numExchange
+        return counts
+        
