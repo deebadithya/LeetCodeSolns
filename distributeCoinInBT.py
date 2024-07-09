c0cf5ddfcfc60f869a2dc8a0c1986896853ccc79
[deeba adithya] <2016016@saec.ac.in>
2024-07-09 08:17:00 +0530

    adding missing uploads

--- distributeCoinInBT.py
+++ distributeCoinInBT.py
@@ -2,7 +2,6 @@
 
 979. Distribute Coins in Binary Tree
 
-
 You are given the root of a binary tree with n nodes where each node in the tree has node.val coins. There are n coins in total throughout the whole tree.
 
 In one move, we may choose two adjacent nodes and move one coin from one node to another. A move may be from parent to child, or from child to parent.
