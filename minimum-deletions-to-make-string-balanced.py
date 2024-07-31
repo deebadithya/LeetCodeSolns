4a3fb9d6681819aed3205e806913049bbee2da53
[deeba adithya] <2016016@saec.ac.in>
2024-07-31 05:41:23 +0530

    adding minimum-deletions-to-make-string-balanced.py

---
+++ minimum-deletions-to-make-string-balanced.py
@@ -0,0 +1,27 @@
+"""
+minimum-deletions-to-make-string-balanced.py
+1653. Minimum Deletions to Make String Balanced
+Solved
+Medium
+Topics
+Companies
+Hint
+You are given a string s consisting only of characters 'a' and 'b'​​​​.
+
+You can delete any number of characters in s to make s balanced. s is balanced if there is no pair of indices (i,j) such that i < j and s[i] = 'b' and s[j]= 'a'.
+
+Return the minimum number of deletions needed to make s balanced.
+"""
+
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        n = len(s)
+        f = [0] * (n + 1)
+        b = 0
+        for i, c in enumerate(s, 1):
+            if c == 'b':
+                f[i] = f[i - 1]
+                b += 1
+            else:
+                f[i] = min(f[i - 1] + 1, b)
+        return f[n]
