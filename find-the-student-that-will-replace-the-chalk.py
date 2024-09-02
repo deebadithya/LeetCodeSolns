e6026755a9568ea7bb6db3aec115b0a6e7ef2e37
[deeba adithya] <2016016@saec.ac.in>
2024-09-02 21:36:21 +0530

    optimized find-the-student-that-will-replace-the-chalk.py

--- find-the-student-that-will-replace-the-chalk.py
+++ find-the-student-that-will-replace-the-chalk.py
@@ -6,13 +6,21 @@ from typing import List
 class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
         sm_chalk = sum(chalk)
-        j = 1
-        while True:
-            if j * sm_chalk < k:
-                k -= (sm_chalk * j)
-                j+=1
-            else:
-                for i in range(len(chalk)):
-                    if k < chalk[i]:
-                        return i
-                    k -= chalk[i]
+        k %= sm_chalk
+
+        for i in range(len(chalk)):
+            if k < chalk[i]:
+                return i
+            k -= chalk[i]
+
+        # sm_chalk = sum(chalk)
+        # j = 1
+        # while True:
+        #     if j * sm_chalk < k:
+        #         k -= (sm_chalk * j)
+        #         j+=1
+        #     else:
+        #         for i in range(len(chalk)):
+        #             if k < chalk[i]:
+        #                 return i
+        #             k -= chalk[i]
